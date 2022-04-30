a09b65cb1f2cb24fd463a520c7322b38816c6b09
BenBariSouf <benbarisoufiane@gmail.com>
2022-04-30 16:58:02 +0300

    update authentication callback method to use strict equality comparison for the response status

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ app.get("/callback", (req, res) => {
 		},
 	})
 		.then((response) => {
-			if (response.status == 200) {
+			if (response.status === 200) {
 				const { access_token, refresh_token, expires_in } = response.data;
 
 				const queryParams = querystring.stringify({
