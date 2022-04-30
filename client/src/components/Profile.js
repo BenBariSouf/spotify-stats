43cf96385d23a178297754b33dd68b1e3ae7ae60
BenBariSouf <benbarisoufiane@gmail.com>
2022-04-30 16:59:35 +0300

    setup react router to handle navigation

---
+++ client/src/components/Profile.js
@@ -0,0 +1,11 @@
+export default function Profile() {
+	return (
+		<div>
+			<h2>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, eveniet quam eos ratione tenetur, facilis, autem qui rem temporibus ipsa a aut similique! Facilis
+				beatae obcaecati quaerat amet eaque quas.
+			</h2>
+			Profile
+		</div>
+	);
+}
