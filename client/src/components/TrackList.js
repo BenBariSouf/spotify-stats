c9a7be9ad87d8893ab08e80a96aefa283c4b2089
BenBariSouf <benbarisoufiane@gmail.com>
2022-05-06 17:49:56 +0300

    create reusable components

---
+++ client/src/components/TrackList.js
@@ -0,0 +1,60 @@
+import { formatDuration } from "../utils";
+import { StyledTrackList } from "../styles";
+import { FaHeart } from "react-icons/fa";
+
+const TrackList = ({ tracks, savedTracks = [] }) => (
+	<>
+		{tracks && tracks.length ? (
+			<StyledTrackList>
+				{tracks.map((track, index) => (
+					<li className="track__item" key={index}>
+						<div className="track__item__num">{index + 1}</div>
+						<div className="track__item__title-group">
+							{track.album.images.length && track.album.images[2] && (
+								<div className="track__item__img">
+									<img src={track.album.images[2].url} alt={track.name} />
+								</div>
+							)}
+							<div className="track__item__name-artist">
+								<div className="track__item__name overflow-ellipsis">{track.name}</div>
+								<div className="track__item__artist overflow-ellipsis">
+									{track.artists.map((artist, index) => (
+										<span key={index}>
+											{artist.name}
+											{index !== track.artists.length - 1 && ", "}
+										</span>
+									))}
+								</div>
+							</div>
+							{savedTracks.includes(track.id) ? (
+								<div className="tooltip">
+									<span className="tooltiptext">Liked</span>
+									<FaHeart color="#1db954" className="track__item__saved__sm" />
+								</div>
+							) : (
+								<span />
+							)}
+						</div>
+						<div className="track__item__album overflow-ellipsis">{track.album.name}</div>
+
+						<div className="track__item__details">
+							{savedTracks.includes(track.id) ? (
+								<div className="tooltip">
+									<span className="tooltiptext">Liked</span>
+									<FaHeart color="#1db954" className="track__item__saved" />
+								</div>
+							) : (
+								<span />
+							)}
+							<div className="track__item__duration">{formatDuration(track.duration_ms)}</div>
+						</div>
+					</li>
+				))}
+			</StyledTrackList>
+		) : (
+			<p className="empty-notice">No tracks available</p>
+		)}
+	</>
+);
+
+export default TrackList;
---
+++ client/src/components/index.js
@@ -0,0 +1,8 @@
+import ArtistsGrid from "./ArtistsGrid";
+import PlaylistsGrid from "./PlaylistsGrid";
+import SectionWrapper from "./SectionWrapper";
+import Loader from "./Loader";
+import TrackList from "./TrackList";
+import TimeRangeButtons from "./TimeRangeButtons";
+
+export { ArtistsGrid, PlaylistsGrid, SectionWrapper, Loader, TrackList, TimeRangeButtons };
