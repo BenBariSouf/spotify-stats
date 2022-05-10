708a2bc10766205dd8ea18e6e84b37e94c950244
BenBariSouf <benbarisoufiane@gmail.com>
2022-05-10 18:56:17 +0300

    relocate app.js to correct folder/file structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import styled from "styled-components/macro";
+import { GlobalStyle } from "./styles";
+import { logout, access_token } from "./spotify";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Login, Profile, TopArtists, Artist, TopTracks, Playlists, Playlist, Track } from "./pages";
+
+const StyledLogoutButton = styled.button`
+	position: absolute;
+	top: var(--spacing-lg);
+	right: var(--spacing-md);
+	padding: var(--spacing-sm) var(--spacing-md);
+	background-color: rgba(0, 0, 0, 0.7);
+	color: var(--white);
+	font-size: var(--fz-xxs);
+	letter-spacing: 1px;
+	font-weight: 600;
+	border-radius: var(--border-radius-pill);
+	z-index: 10;
+	text-transform: uppercase;
+	@media (min-width: 768px) {
+		right: var(--spacing-lg);
+	}
+
+	&:hover {
+		cursor: pointer;
+		color: var(--black);
+		background-color: var(--white);
+	}
+`;
+
+// Scroll to top of page when changing routes
+// https://reactrouter.com/web/guides/scroll-restoration/scroll-to-top
+function ScrollToTop() {
+	const { pathname } = useLocation();
+
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, [pathname]);
+
+	return null;
 }
+const App = () => {
+	const [token, setToken] = useState(null);
+	useEffect(() => {
+		setToken(access_token);
+	}, []);
+
+	return (
+		<div className="app">
+			<GlobalStyle />
+
+			{!token ? (
+				<Login />
+			) : (
+				<>
+					<StyledLogoutButton onClick={logout}>Logout</StyledLogoutButton>
+
+					<ScrollToTop />
+					<Routes>
+						<Route path="/" element={<Profile />} />
+						<Route path="/top-artists" element={<TopArtists />} />
+						<Route path="/artists/:id" element={<Artist />} />
+						<Route path="/top-tracks" element={<TopTracks />} />
+						<Route path="/playlists" element={<Playlists />} />
+						<Route path="/playlists/:id" element={<Playlist />} />
+						<Route path="/tracks/:id" element={<Track />} />
+					</Routes>
+				</>
+			)}
+		</div>
+	);
+};
 
 export default App;
